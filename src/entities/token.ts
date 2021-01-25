a950ebbe3563f8d2b7c961bad5c772b8919df38e
blong <blongcash@hotmail.com>
2021-01-25 14:56:14 +0800

    upgrade sdk version to 1.0.2

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -79,5 +79,7 @@ export const WETH = {
     'Wrapped Ether'
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether')
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.HET]: new Token(ChainId.HET, '0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.HEM]: new Token(ChainId.HEM, '0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f', 18, 'WETH', 'Wrapped Ether')
 }
